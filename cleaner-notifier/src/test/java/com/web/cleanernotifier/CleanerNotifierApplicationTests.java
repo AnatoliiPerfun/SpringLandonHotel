9ce8673eac2f6b290b91eb548b327174c1de5460
AnatoliiPerfun <tolikvik@gmail.com>
2022-07-24 20:21:55 +0300

    amqp producer config added

---
+++ cleaner-notifier/src/test/java/com/web/cleanernotifier/CleanerNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.cleanernotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanerNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
